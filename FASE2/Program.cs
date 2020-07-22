06f39cf5e9f9c2ef18b04326637d813c5146462d
LAPTOP-HOVLHHNQ\oavil <oavila1971@gmail.com>
2020-07-22 13:31:29 +0200

    Fase2 i Fase3 de l'exercici 1

---
+++ FASE2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FASE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int ANY1948 = 1948;
+            var freqTraspas = 4;
+            int numTraspas;
+
+            Console.WriteLine("Introdueix el teu any de naixement:");
+            int anyNaix = int.Parse(Console.ReadLine());
+
+            numTraspas = (anyNaix - ANY1948) / freqTraspas;
+
+            Console.WriteLine($"Han passat {numTraspas} anys de traspas des del teu naixement");
+        }
+    }
+}
---
+++ FASE3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FASE3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anyIni = 1948;
+            var anyNaix = 1971;
+            const int freqTrasp = 4;
+            bool esTrasp;
+            var cadSiTrasp = "El meu any de naixement SÍ és de traspàs";
+            var cadNoTrasp = "El meu any de naixement NO és de traspàs";
+            var nom = "Òscar Àvila Castilla";
+            var dataNaix = "18/02/" + anyNaix;
+            string resposta;
+
+            esTrasp = false;
+            
+
+            for (int i = anyIni; i <= anyNaix; i++)
+            {
+                
+                if (i % freqTrasp == 0)
+                {
+                    Console.WriteLine("Any traspàs: " + i);
+                    esTrasp = true;
+                }
+
+                esTrasp = false;
+
+
+            }
+
+            if (esTrasp == true)
+            {
+                resposta = cadSiTrasp;
+            }
+            else 
+            {
+                resposta = cadNoTrasp;
+            }
+            Console.WriteLine("El meu nom és: " + nom);
+            Console.WriteLine("Vaig néixer el dia " + dataNaix);
+            Console.WriteLine(resposta);
+        }
+    }
+}
