a0fb37292cc3f2739843b6fa5ac9e68bb49094e1
LAPTOP-HOVLHHNQ\oavil <oavila1971@gmail.com>
2020-07-22 12:58:28 +0200

    Exercici 1 de l'itinerari .NET

---
+++ FASE1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Dynamic;
+
+namespace FASE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Introdueix nom");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Introdueix primer cognom");
+            string cognom1 = Console.ReadLine();
+            Console.WriteLine("Introdueix segon cognom");
+            string cognom2 = Console.ReadLine();
+
+            Console.WriteLine("Introdueix dia de naixement");
+            int dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdueix mes de naixament");
+            int mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdueix any de naixement");
+            int any = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Nom complet: {cognom1} {cognom2}, {nom}");
+            Console.WriteLine($"Data de naixament: {dia}/{mes}/{any}");
+        }
+    }
+}
